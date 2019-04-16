93491c8944cfc3c18c2a45c7f42186e435f66fbe
deepti.penmetsa <deepti.penmetsa@bluepal.com>
2019-04-16 14:12:44 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
-// import {Navbar, NavbarBrand} from 'reactstrap';
-import MainComponent from './components/MainComponent'
+import MainComponent from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-//import {DISHES} from './shared/dishes'
 
 class App extends Component {
-
-  // constructor(props){
-  //   super(props);
-
-  //   this.state={
-  //     dishes: DISHES
-  //   }
-  // }
   render() {
+
     return (
-      // <div>
-      //   <Navbar dark color="primary">
-      //   <div className="container">
-      //   <NavbarBrand href="/">Ristorante co fusion</NavbarBrand>
-      //   </div>
-      //   </Navbar>
-      //   {/* <Menu /> */}
-      //   <Menu dishes={this.state.dishes} />
-      // </div>
-      <div><MainComponent /></div>
+      <BrowserRouter>
+      <div className="App">
+      <MainComponent />
+      </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import DishdetailComponent from './DishdetailComponent';
 import {DISHES} from '../shared/dishes'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
+import Home from './HomeComponent'
+import {Switch, Route, Redirect} from 'react-router-dom' 
 
 class MainComponent extends Component {
 
@@ -16,16 +17,22 @@ class MainComponent extends Component {
     }
   }
 
-  onDishSelect(dishId){
-    this.setState({selectedDish:dishId});
-}
-
   render() {
+
+    const HomePage = () => {
+      return(
+          <Home/>
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)}/>
-        <DishdetailComponent dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]}/>
+        <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
+            <Redirect to="/home"></Redirect>
+        </Switch>
         <Footer />
       </div>
     );
