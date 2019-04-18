1c14ad2ff186abdaeca9fd6027ebb543a334b0f9
deepti.penmetsa <deepti.penmetsa@bluepal.com>
2019-04-18 15:53:31 +0530

    Animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish({ dish }) {
     if (dish != null)
         return (
             <div className="col-12 col-md-5 m-1">
+             <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
             <Card>
                 <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
@@ -16,6 +22,7 @@ function RenderDish({ dish }) {
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
             </div>
         );
     else
@@ -30,14 +37,18 @@ function RenderComments({ comments, postComment, dishId}) {
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
+                <Stagger in>
                     {comments.map((cmt) => {
                         return (
+                            <Fade in>
                             <li key={cmt.id}>
                                 <p>{cmt.comment}</p>
                                 <p>--{cmt.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(cmt.date)))}</p>
                             </li>
+                            </Fade>
                         )
                     })}
+                </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
